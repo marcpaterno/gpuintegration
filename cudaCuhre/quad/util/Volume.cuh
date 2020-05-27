a12624a75857bf23a3699b66623137799d496f00
Marc Paterno <paterno@fnal.gov>
2020-05-27 09:02:47 -0500

    Put template Volume into namespace quad

--- cudaCuhre/quad/util/Volume.cuh
+++ cudaCuhre/quad/util/Volume.cuh
@@ -1,30 +1,29 @@
 #ifndef CUDACUHRE_QUAD_GPUQUAD_VOLUME_CUH
 #define CUDACUHRE_QUAD_GPUQUAD_VOLUME_CUH
 
+#include <cstring>
+
 // user must make sure to call cudaMalloc and cudaMemcpy regarding d_highs and
 // d_lows
 
-template <typename T, int NDIM>
-struct Volume {
+namespace quad {
+  template <typename T, int NDIM>
+  struct Volume {
 
-  T highs[NDIM];
-  T lows[NDIM];
+    T highs[NDIM] = {0.0};
+    T lows[NDIM] = {1.0};
 
-  __host__
-  Volume()
-  {
-	  for(int i=0; i<NDIM; i++){
-		 highs[i]=1;
-		 lows[i] =0;
-	  }
-  }
+    __host__
+    Volume()
+    {}
 
-  __host__ __device__
-  Volume(T const* l, T const* h)
-  {
-    memcpy(lows, l, NDIM * sizeof(T));
-    memcpy(highs, h, NDIM * sizeof(T));
-  }
-};
+    __host__ __device__
+    Volume(T const* l, T const* h)
+    {
+      std::memcpy(lows, l, NDIM * sizeof(T));
+      std::memcpy(highs, h, NDIM * sizeof(T));
+    }
+  };
+}
 
 #endif
