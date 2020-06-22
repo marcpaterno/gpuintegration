5d2dbddd145969bd1fb3ec9ce03ddffa9ec1f9f0
Marc Paterno <paterno@fnal.gov>
2020-06-22 16:46:24 -0500

    Move Genz function 1 (absolute value, 5d) to its own header.

--- cudaCuhre/tests/genz_1abs_5d.cu
+++ cudaCuhre/tests/genz_1abs_5d.cu
@@ -1,4 +1,4 @@
-
+#include "genz_1abs_5d.cuh"
 #include "function.cuh"
 #include "quad/quad.h"
 #include "quad/util/cudaUtil.h"
@@ -11,26 +11,6 @@
 #include <iomanip>
 #include <iostream>
 
-// From Mathematica 12.1 Integration, symbolic integration over unit hypercube.
-// This is the multiplier that gives genz_1_8d an integrated value of 1 over the
-// unit hypercube.
-using std::sin;
-using std::cos;
-using std::abs;
-
-double constexpr integral = 6.371054e-01; // Value is approximate
-double constexpr normalization = 1./integral;
-
-struct Genz_1abs_5d {
-  __device__ __host__
-    Genz_1abs_5d () { };
-
-  __device__ __host__ double
-    operator() (double v, double w, double x, double y, double z)
-  {
-    return normalization * abs(cos(4.*v + 5.*w + 6.*x + 7.*y + 8.*z));
-  }
-};
 
 // Integrate the provided integrand using quad::Cuhre, to the given relative
 // error tolerance, writing out timing information to std::cout.
