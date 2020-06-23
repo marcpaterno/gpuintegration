b2dda5b6123c20a4d21afd7c8c35e75b336f4b6e
Marc Paterno <paterno@fnal.gov>
2020-06-22 20:18:55 -0500

    Add start of convergence testing program.

---
+++ cudaCuhre/tests/test_cuhre_convergence.cu
@@ -0,0 +1,29 @@
+#include "catch2/catch.hpp"
+#include "../quad/GPUquad/Cuhre.cuh"
+
+#include "genz_1abs_5d.cuh"
+
+
+TEST_CASE("genz_1abs_5d")
+{
+  SECTION("decreasing epsrel results in decreasing error estimate")
+  {
+    double epsrel = 1.0e-3;  // starting error tolerance.
+
+    double constexpr epsabs = 1.0e-40;
+
+    double lows[] =  {0., 0., 0., 0., 0.};
+    double highs[] = {1., 1., 1., 1., 1.};
+    constexpr int ndim = 5;
+    quad::Volume<double, ndim> vol(lows, highs);
+    quad::Cuhre<double, ndim> alg(0, nullptr, 0, 0, 1);
+
+    Genz_1abs_5d integrand;
+ 
+    auto const res = alg.integrate(integrand, epsrel, epsabs, &vol);
+    CHECK(res.status == true);
+    double const ratio = res.error/(epsrel * res.value);
+    CHECK(ratio <= 1.0);
+  }
+};
+
