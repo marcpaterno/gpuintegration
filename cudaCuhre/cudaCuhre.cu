b10dec384ce862cfd3e845aceded8b7b1cdd3c5d
Ioannis Sakiotis <isaki001@odu.edu>
2020-07-22 13:59:46 -0400

    calling with new options

--- cudaCuhre/cudaCuhre.cu
+++ cudaCuhre/cudaCuhre.cu
@@ -15,45 +15,6 @@ using std::chrono::duration;
 
 constexpr double EPSABS = 1e-12;
 
-class GENZ_1_8d{
-	
-	public:
-	double normalization;
-	double integral;
-	__device__ __host__
-	GENZ_1_8d(){
-		integral = (1./315.) * sin(1.) * sin(3./2.) * sin(2.) * sin (5./2.) * sin(3.) *
-                        sin(7./2.) * sin(4.) * (sin(37./2.) - sin(35./2.));
-		normalization = 1./integral;
-						
-	}
-	__device__ __host__ double
-	operator()(double s, double t, double u, double v,
-                 double w, double x, double y, double z){
-		return normalization * cos(s + 2.*t + 3.*u + 4.*v + 5.*w + 6.*x + 7.*y + 8.*z);			 
-	}
-};
-
-/*class MARC_2_6d{
-	public:
-	__device__ __host__ double
-	operator()(double u, double v, double w, double y, double x, double z){
-		double f = (12/(7 - 6*pow(log(2),2) + log(64)))*(u*v+(pow(w,y)*x*y)/(1+u)+pow(z,2));
-		if(f<0)
-			printf("negative\n");
-		return f;
-	}
-};*/
-
-class absCosSum5DWithoutK{
-	// ACTUAL ANSWER = 0.6371054
-	public:
-		__device__ __host__ double
-		operator()(double v, double w, double x, double y, double z){
-			return fabs(cos(4.*v + 5.*w + 6.*x + 7.*y + 8.*z));
-		}
-	
-};
 
 int
 main(int argc, char** argv)
@@ -72,7 +33,7 @@ main(int argc, char** argv)
     exit(0);
   }
 	
-  TYPE epsrel = 1e-4;
+  TYPE epsrel = 1e-7;
   if (args.CheckCmdLineFlag("e")) {
     args.GetCmdLineArgument("e", epsrel);
   }
@@ -91,25 +52,43 @@ main(int argc, char** argv)
  // Initialize device
   QuadDebugExit(args.DeviceInit());
  
-  constexpr int ndim = 5;
+  constexpr int ndim = 8;
   
   Cuhre<TYPE, ndim> cuhre(argc, argv, 0, verbose, numDevices);
 		
-  absCosSum5D integrand;
-  int _final 			= 0;
-  int outfileVerbosity  = 0;
-  int phase_I_type 		= 1; // alternative phase 1
+  BoxIntegral8_22 integrand;
+  int _final 			= 1;
+  int outfileVerbosity  = 4;
+  int phase_I_type 		= 0; // alternative phase 1
+  double true_value 	= 8879.851175413485;
+  std::string id 		= "BoxIntegral8_15";
   
-  double highs[ndim] = {1, 1, 1, 1, 1};
-  double lows[ndim] =  {0, 0, 0, 0, 0};
+  double highs[ndim] = {1, 1, 1, 1, 1, 1, 1, 1};
+  double lows[ndim] =  {0, 0, 0, 0, 0, 0, 0, 0};
   Volume<double, ndim> vol(lows, highs);
   
   using MilliSeconds = std::chrono::duration<double, std::chrono::milliseconds::period>;
-	
   auto t0 = std::chrono::high_resolution_clock::now();
-  cuhreResult result = cuhre.integrate<absCosSum5D>(integrand, epsrel, EPSABS, &vol, outfileVerbosity, _final, phase_I_type);
+  cuhreResult result = cuhre.integrate<BoxIntegral8_22>(integrand, epsrel, EPSABS, &vol, outfileVerbosity, _final, phase_I_type);
   MilliSeconds dt = std::chrono::high_resolution_clock::now() - t0;
-  printf("%.15f \t %.15f \t %lu %i\n", result.value, result.error, result.nregions, result.status);
-  std::cout<<"Time in ms:"<< dt.count()<<std::endl;
+  
+  //printf("%s, %f, %f, %.15f,%.15f,%lu, %i, %f\n", id.c_str() , result.value, epsrel, EPSABS, result.error, result.nregions, result.status, dt.count());
+  printf("%.15f +- %.15f nregions:%i flag:%i time:%f\n", result.value, result.error, result.nregions, result.status, dt.count());
+  std::stringstream outfile;
+  outfile<<"id, epsrel, epsabs, value, error, regions, flag, final, total_time, absolute_error, true_relative_error"<<std::endl;
+  outfile<<id<<","
+  		 <<epsrel<<","
+		 <<EPSABS<<","
+		 <<result.value<<","
+		 <<result.error<<","
+		 <<result.nregions<<","
+		 <<result.status<<","
+		 <<_final<<","
+		 <<dt.count()<<","
+		 <<abs(true_value-result.value)<<","
+		 <<abs((true_value-result.value)/true_value)<<","
+		 <<std::endl;
+		 
+  PrintToFile(outfile.str(), id + ".csv");
   return 0;
 }
