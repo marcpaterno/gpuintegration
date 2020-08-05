b894a2b8c2521fba7ecbc65420f0794022cb08b6
Ioannis Sakiotis <isaki001@odu.edu>
2020-08-05 14:25:02 -0400

    added toy kernel for cpu gpu integrand comparison

--- cudaCuhre/cudaCuhre.cu
+++ cudaCuhre/cudaCuhre.cu
@@ -15,6 +15,13 @@ using std::chrono::high_resolution_clock;
 
 constexpr double EPSABS = 1e-12;
 
+__global__ void
+testKernel(){
+	FUNC2 test_integrand;
+	printf("GPU RESULT %a\n",    test_integrand(0x1.f4b65783633c5p-1, 0x1.f4b65783633c5p-1, 0x1p-1, 0x1p-1, 0x1p-1, 0x1p-1, 0x1p-1, 0x1.69350f939876p-6));
+	printf("GPU RESULT %.17f\n", test_integrand(0x1.f4b65783633c5p-1, 0x1.f4b65783633c5p-1, 0x1p-1, 0x1p-1, 0x1p-1, 0x1p-1, 0x1p-1, 0x1.69350f939876p-6));
+}
+
 int
 main(int argc, char** argv)
 {
@@ -32,7 +39,7 @@ main(int argc, char** argv)
     exit(0);
   }
 
-  TYPE epsrel = 1.0e-3;
+  TYPE epsrel = 1.0e-4;
   if (args.CheckCmdLineFlag("e")) {
     args.GetCmdLineArgument("e", epsrel);
   }
@@ -51,28 +58,36 @@ main(int argc, char** argv)
   // Initialize device
   QuadDebugExit(args.DeviceInit());
 
-  constexpr int ndim = 8;
+  constexpr int ndim = 5;
 
   Cuhre<TYPE, ndim> cuhre(argc, argv, 0, verbose, numDevices);
-
-  BoxIntegral8_22 integrand;
+  absCosSum5DWithoutKPlus1 integrand;
   int _final = 0;
   int outfileVerbosity = 0;
   int phase_I_type = 0; // alternative phase 1
-  // double true_value 	= .999926247661939;
-  std::string id = "Genz_1abs_5d";
-
-  double highs[ndim] = {1., 1., 1., 1., 1., 1., 1., 1.};
-  double lows[ndim] = {0., 0., 0., 0., 0., 0., 0., 0.};
+	
+  double highs[ndim] = {1., 1., 1., 1., 1.};
+  double lows[ndim]  = {0., 0., 0., 0., 0.};
   Volume<double, ndim> vol(lows, highs);
-
+  double true_value = 0.999926247661939;
+  
   using MilliSeconds =
     std::chrono::duration<double, std::chrono::milliseconds::period>;
   auto t0 = std::chrono::high_resolution_clock::now();
-  cuhreResult result = cuhre.integrate<BoxIntegral8_22>(
+  cuhreResult result = cuhre.integrate<absCosSum5DWithoutKPlus1>(
     integrand, epsrel, EPSABS, &vol, outfileVerbosity, _final, phase_I_type);
   MilliSeconds dt = std::chrono::high_resolution_clock::now() - t0;
-
+	
+  std::cout.precision(17);
+  std::cout<<true_value<<",\t"
+			<<epsrel<<",\t"
+			<<EPSABS<<",\t"
+			<<result.value<<",\t"
+			<<result.error<<",\t"
+			<<result.nregions<<",\t"
+			<<result.status<<",\t"
+			<<_final<<",\t"
+			<<dt.count()<<std::endl;	
   printf("%.15f +- %.15f epsrel:%f, nregions:%lu flag:%i time:%f\n",
          result.value,
          result.error,
