fe5b50fed77a77dcf5f11a36e15338d00cf49aef
Marc Paterno <paterno@fnal.gov>
2020-10-14 09:12:26 -0500

    More printout in hello program

--- cudaCuhre/tests/hello.cu
+++ cudaCuhre/tests/hello.cu
@@ -5,6 +5,9 @@ __global__ void cuda_hello() {
 }
 
 int main() {
+  printf("Before calling the kernel\n");
   cuda_hello<<<1,1>>>(); 
+  cudaDeviceSynchronize();
+  printf("After calling the kernel\n");
   return 0;
 }
