87da0b23f37e0a6861e1ee51f7630cbd9503b741
isaki001 <isaki001@odu.edu>
2021-05-26 21:24:27 -0400

    setting blocks to 256 from 32

--- cudaPagani/quad/quad.h
+++ cudaPagani/quad/quad.h
@@ -2,10 +2,10 @@
 #define CUDACUHRE_QUAD_QUAD_h
 
 #define TIMING_DEBUG 1
-//#define BLOCK_SIZE 256
+#define BLOCK_SIZE 256
 //#define BLOCK_SIZE 128
 //#define BLOCK_SIZE 64
-#define BLOCK_SIZE 32
+//#define BLOCK_SIZE 32
 #define SM_REGION_POOL_SIZE 128
 
 #define GLOBAL_ERROR 1
