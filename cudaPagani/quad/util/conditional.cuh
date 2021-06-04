906d96150958ef65b4e9fe2569b27d3124de5557
isaki001 <isaki001@odu.edu>
2021-06-04 10:32:04 -0400

    missing file

---
+++ cudaPagani/quad/util/conditional.cuh
@@ -0,0 +1,25 @@
+#ifndef CONDITIONAL_H
+#define CONDITIONAL_H
+
+#include "quad/GPUquad/Interp1D.cuh"
+#include "quad/GPUquad/Interp2D.cuh"
+#include "quad/GPUquad/Polynomial.cuh"
+
+struct GPU {
+  template<size_t order>
+  using polynomial = quad::polynomial<order>;
+  typedef quad::Interp2D Interp2D;
+  typedef quad::Interp1D Interp1D;
+  //typedef quad::ez ez;
+};
+
+
+struct CPU {
+    //commented out temporarily to keep y3_cluster independent from PAGANI
+  //template<size_t order>
+  //using polynomial = y3_cluster::polynomial<order>;
+  //typedef y3_cluster::Interp2D Interp2D;
+  //typedef y3_cluster::Interp1D Interp1D;
+};
+
+#endif
