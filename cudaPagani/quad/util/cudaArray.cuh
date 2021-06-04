0898cd74ef44e195d8003a5a831a8ba013314cf3
isaki001 <isaki001@odu.edu>
2021-06-04 07:07:11 -0400

    StatefulIntegrand test

--- cudaPagani/quad/util/cudaArray.cuh
+++ cudaPagani/quad/util/cudaArray.cuh
@@ -32,6 +32,77 @@ namespace gpu {
 
     T data[s];
   };
+
+  template <typename T>
+  class cudaDynamicArray {
+  public:
+  
+    cudaDynamicArray(){
+      printf("called dynamic array user constuctor\n");
+       data = nullptr;
+       N = 0;
+    }
+    
+    // host-only function
+    void
+    Initialize(T const* initData, size_t s)
+    {
+      N = s;
+      cudaMallocManaged((void**)&data, sizeof(T) * s);
+      cudaMemcpy(data, initData, sizeof(T) * s, cudaMemcpyHostToDevice);
+    }
+    
+    __host__ __device__
+    ~cudaDynamicArray() {
+      #ifndef __CUDACC__
+      cudaFree(data);
+      #endif
+   }
+
+    __host__ __device__ const T*
+    begin() const
+    {
+      return &data[0];
+    }
+
+    __host__ __device__ const T*
+    end() const
+    {
+      return (&data[0] + N);
+    }
+
+    __host__ __device__  constexpr std::size_t
+    size() const
+    {
+      return N;
+    }
+
+    cudaDynamicArray&
+    operator=(const cudaDynamicArray& source)
+    {
+      printf("Inside operator=\n");
+      printf("Calling operator= on object at %p copying %lu objects\n", this, source.size());
+      cudaMallocManaged((void**)&data, sizeof(T) * source.size());
+      cudaMemcpy(data, source.data, sizeof(T) * N, cudaMemcpyHostToDevice);
+      return *this;
+    }
+
+    __host__ __device__ T&
+    operator[](std::size_t i)
+    {
+      return data[i];
+    }
+
+    __host__ __device__ T
+    operator[](std::size_t i) const
+    {
+      return data[i];
+    }
+
+    T* data;
+    size_t N;
+  }; // cudaDynamicArray
+
 };
 
 #endif
--- cudaPagani/tests/StatefulIntegrand.cu
+++ cudaPagani/tests/StatefulIntegrand.cu
@@ -10,6 +10,63 @@
 
 using namespace quad;
 
+
+template <class M>
+M
+make_from_file(char const* filename)
+{
+  static_assert(std::is_default_constructible<M>::value, "Type must be default constructable");
+  char const* basedir =  std::getenv("PAGANI_DIR");
+  std::string fname(basedir);
+  fname += "/tests/";
+  fname += filename;
+  std::cout<<"Filename:"<<fname<<std::endl;
+  std::ifstream in(fname);
+  if (!in) {
+    std::string msg("Failed to open file: ");
+    msg += fname;
+    throw std::runtime_error(msg);
+  }
+  
+  M result;
+  in >> result;
+  return result;
+}
+
+
+struct ToyModelWithHeapArray{
+  ToyModelWithHeapArray(int const* d, size_t s) {data.Initialize(d, s);}
+  
+  ToyModelWithHeapArray() = default;
+  
+  __host__ __device__ double
+  operator()(double x, double y) const
+  {
+    double answer = data[0] + x * data[1] + y * data[2] + x*y*data[3];
+    return answer;
+  }
+  
+  gpu::cudaDynamicArray<int> data; 
+};
+
+struct ToyIntegrandWithHeapModel {
+
+  explicit
+  ToyIntegrandWithHeapModel(int* const modeldata, size_t s) :
+     model(modeldata, s)
+{ }
+  
+  __host__ __device__ double
+  operator()(double x, double y) const {
+    printf("inside ToyIntegrandWithHeapModel operator()\n");
+    printf("%i, %i, %i, %i, %i\n", model.data[0], model.data[1], model.data[2], model.data[3], model.data[4]);
+    return model(x, y);
+  }
+  
+  double true_value = 34.0;
+  ToyModelWithHeapArray model;
+};
+
 struct ToyModel {
 
   explicit
@@ -18,14 +75,14 @@ struct ToyModel {
   }
 
   ToyModel() = default;
-
+  
   __host__ __device__ double
   operator()(double x, double y) const
   {
     double answer = data[0] + x * data[1] + y * data[2] + x*y*data[3];
     return answer;
   }
-
+  
   gpu::cudaArray<int, 5>data;
 }; // ToyModel
 
@@ -75,10 +132,18 @@ toy_integration_algorithm(IntegT const& on_host)
 
 TEST_CASE("State of Integrand is std::array"){
     std::array<int, 5> attributes { 5, 4, 3, 2, 1};
-    ToyModel my_model(attributes.data());
     ToyIntegrand my_integrand(attributes);
     auto [result, rc]  = toy_integration_algorithm(my_integrand);
     
+    CHECK(result == 34.0);
+    CHECK(result == my_integrand.true_value);
+}
+
+TEST_CASE("Dynamic Array"){
+    std::array<int, 5> attributes { 5, 4, 3, 2, 1};
+    ToyIntegrandWithHeapModel my_integrand(attributes.data(), attributes.size());
+    auto [result, rc]  = toy_integration_algorithm(my_integrand);
+    std::cout<<"Result:"<<result<<"\n";
     CHECK(result == 34.0);
     CHECK(result == my_integrand.true_value);
 }
