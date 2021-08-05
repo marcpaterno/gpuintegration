cd0f8aa327a7df45b1d7e4888f4c15d7398c4d8e
Marc Paterno <paterno@fnal.gov>
2021-08-05 11:10:22 -0500

    Remove execute bit from some headers

--- cudaPagani/tests/model.hh
+++ cudaPagani/tests/model.hh
@@ -1,11 +1,6 @@
 #ifndef MODEL_HH
 #define MODEL_HH
 
-#include <cmath>
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <array>
 #include <vector>
 
 std::vector<double> cpuExecute();
