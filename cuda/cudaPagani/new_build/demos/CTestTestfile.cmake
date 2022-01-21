e83a2b72c258a8a90e9aab17779f2528d17df2b1
Marc Paterno <paterno@fnal.gov>
2022-01-21 12:46:03 -0600

    Move accidentally added build results

--- cuda/cudaPagani/new_build/demos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/isaki001/numint/gpuintegration/cudaPagani/demos
-# Build directory: /home/isaki001/numint/gpuintegration/cudaPagani/new_build/demos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
