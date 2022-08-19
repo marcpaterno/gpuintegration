65473f382df8fe3c59674d62f64ce7b817b9a6cb
isaki001 <isaki001@odu.edu>
2022-08-19 11:15:22 -0400

    remove unneeded code from Interp2D test

--- cuda/pagani/tests/Interpolation2D.cu
+++ cuda/pagani/tests/Interpolation2D.cu
@@ -161,23 +161,6 @@ TEST_CASE("Interp2D on bilinear")
   
   double interpResult = Evaluate(f, 2.5, 1.5);
   CHECK(interpResult == 4.5);
-
-  
-    constexpr int ndim = 2;
-    quad::Pagani<double, ndim> pagani;
-    std::array<double, ndim> lows = {1., 1.};
-    std::array<double, ndim> highs = {3., 4.};
-    quad::Volume<double, ndim> vol(lows, highs);
-
-    size_t free_physmem, total_physmem;
-    
-    for(int run = 0; run < 100000; ++run){
-      cudaMemGetInfo(&free_physmem, &total_physmem);
-      std::cout << "start" << "," << run << "," << free_physmem << std::endl;
-      pagani.integrate<quad::Interp2D>(f, 1.e-3, 1.e-12, &vol);
-      std::cout << "end" << "," << run << "," << free_physmem << std::endl;
-    }
-  
 }
 
 
