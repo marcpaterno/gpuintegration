f7a342cbf35f344f5c78597c9c94b8aa0f6131f2
isaki001 <isaki001@odu.edu>
2022-09-02 12:18:37 -0400

    added cuda custom reduction headers and test

--- cuda/pagani/tests/Reduction.cu
+++ cuda/pagani/tests/Reduction.cu
@@ -3,13 +3,16 @@
 #include "cuda/pagani/demos/function.cuh"
 #include "cuda/pagani/quad/GPUquad/Sample.cuh"
 #include "cuda/pagani/quad/quad.h"
+#include "cuda/pagani/quad/util/mem_util.cuh"
 #include "cuda/pagani/quad/util/Volume.cuh"
 #include "cuda/pagani/quad/util/cudaUtil.h"
+#include "cuda/pagani/quad/util/custom_functions.cuh"
 #include <chrono>
 #include <cmath>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
+#include <numeric>
 
 using namespace quad;
 
@@ -42,7 +45,7 @@ public:
   }
 };
 
-TEST_CASE("Constant Positive Value Function")
+/*TEST_CASE("Constant Positive Value Function")
 {
   constexpr int ndim = 2;
   size_t numRegions = 16;
@@ -51,9 +54,6 @@ TEST_CASE("Constant Positive Value Function")
   int heuristicID = 0;
   double epsrel = 1.0e-3;
   double epsabs = 1.0e-12;
-  // int key = 0;
-  // int verbose = 0;
-  // int numDevices = 1;
   Pagani<double, 2> pagani;
   cuhreResult res = pagani.integrate<PTest>(integrand, epsrel, epsabs);
 
@@ -63,4 +63,54 @@ TEST_CASE("Constant Positive Value Function")
   // returns are never precisely equal to 0. and 15.37
   printf("ttotalEstimate:%.15f\n", integral);
   CHECK(abs(integral - 15.37) <= .00000000000001);
+}*/
+
+TEST_CASE("Custom Reduction with Atomics"){
+	
+	auto init_vector_and_compute_sum = [=](std::vector<double>& arr, double& val, size_t size){
+		arr.resize(size);
+		std::iota(arr.begin(), arr.end(), val);
+		double true_val = 0.;
+		for(auto& v: arr){
+			true_val += v;
+		}
+		arr.clear();
+		return true_val;
+	};
+	
+	auto test_arrs_with_starting_val = [=](double val){
+		std::vector<double> arr;
+		for(int i = 1; i <= 25; ++i){//probably need to start with size 32 or 64, I doubt it will work for 2, 4, 8, 16
+			const size_t size = pow(2, i);
+			const double true_val = init_vector_and_compute_sum(arr, val, size);
+			double* gpu_copy = cuda_malloc<double>(size);
+			cuda_memcpy_to_device<double>(gpu_copy, arr.data(), size);
+			const double res = custom_reduce_atomics(gpu_copy, size);
+			cudaFree(gpu_copy);
+			CHECK(true_val == Approx(res));
+		}
+	};
+	
+	SECTION("Power 2 array sizes")
+	{
+		test_arrs_with_starting_val(-1000.);
+		test_arrs_with_starting_val(1000.);
+		test_arrs_with_starting_val(1000.5);
+		test_arrs_with_starting_val(1.);
+	}
+	
+	SECTION("Non-power 2 array sizes")
+	{
+		double vec_first_val = 1.;
+		std::vector<double> arr;
+		for(int i = 1; i < 1000; ++i){//probably need to start with size 32 or 64, I doubt it will work for 2, 4, 8, 16
+			size_t size = i + 10;
+			const double true_val = init_vector_and_compute_sum(arr, vec_first_val, size);
+			double* gpu_copy = cuda_malloc<double>(size);
+			cuda_memcpy_to_device<double>(gpu_copy, arr.data(), size);
+			const double res = custom_reduce_atomics(gpu_copy, size);
+			cudaFree(gpu_copy);
+			CHECK(true_val == Approx(res));
+		}
+	}
 }
