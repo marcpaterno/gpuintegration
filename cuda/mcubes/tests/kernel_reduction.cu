c67566a2230ea5434a9dcc5cc985da58a44e8319
Marc Paterno <paterno@fnal.gov>
2022-10-05 15:10:43 -0500

    Enhance test for blockReduceSum

--- cuda/mcubes/tests/kernel_reduction.cu
+++ cuda/mcubes/tests/kernel_reduction.cu
@@ -3,72 +3,68 @@
 #include "cuda/mcubes/util/util.cuh"
 #include "cuda/mcubes/util/vegas_utils.cuh"
 #include "cuda/mcubes/vegasT.cuh"
+#include "cuda/pagani/quad/util/mem_util.cuh"
 #include <array>
 
 __global__ void
-kernel(int32_t totalNumThreads,
-       int LastChunk,
-       double* result_dev,
-       double* block_reductions)
+run_block_reduce_sum(int32_t totalNumThreads,
+                     double* total_sum,
+                     double* block_reductions)
 {
-  uint32_t m = blockIdx.x * blockDim.x + threadIdx.x;
-  uint32_t tx = threadIdx.x;
-  double fbg = 0.; // each block reduction yields 128*1.1 even though the second
-                   // block shouldn'tbe getting it from all
-  if (m < totalNumThreads) {
-    fbg = 1.1;
-  }
+  auto const block_id = blockIdx.x;
+  auto const global_thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+  auto const block_thread_id = threadIdx.x;
 
-  fbg = cuda_mcubes::blockReduceSum(fbg); // i thought issue would be ere
+  double const fbg = (global_thread_id < totalNumThreads) ? 1.1 : 0.0;
+  double const sum = cuda_mcubes::blockReduceSum(fbg);
 
-  if (tx == 0) {
-    block_reductions[blockIdx.x] = fbg;
-    atomicAdd(&result_dev[0],
-              fbg); // i thought this wouldn't be problematic, threads where m >
-                    // totalNumThreads should have zeroes all over
+  if (block_thread_id == 0) {
+    // Write this block's answer to global memory.
+    block_reductions[block_id] = sum;
+    // i thought this wouldn't be problematic, threads where m > totalNumThreads
+    // should have zeroes all over
+    // Every block contributes to result_dev
+    atomicAdd(total_sum, sum);
   }
 }
 
-void
-Reduction(double ncall, int chunkSize, int ndim)
-{
-  Kernel_Params kernel_params(ncall, chunkSize, ndim);
-  double* result = cuda_malloc_managed<double>(1);
-  double* block_reductions =
-    cuda_malloc_managed<double>(static_cast<size_t>(kernel_params.nBlocks));
-  double fgb = 1.1;
-
-  kernel<<<kernel_params.nBlocks, kernel_params.nThreads>>>(
-    kernel_params.totalNumThreads,
-    kernel_params.LastChunk,
-    result,
-    block_reductions);
-  cudaDeviceSynchronize();
-
-  SECTION("Block Reduction")
-  {
-    for (uint32_t i = 0; i < kernel_params.nBlocks - 1; i++) {
-      CHECK(block_reductions[i] == Approx(BLOCK_DIM_X * fgb));
-    }
-  }
-
-  SECTION("Atomic Addition")
-  {
-    CHECK(result[0] == Approx(kernel_params.totalNumThreads * fgb));
-  }
-
-  // check last thread which doesn't have the same chunks
-}
-
-TEST_CASE("Reduction")
+TEST_CASE("Aligned reduction")
 {
   std::array<double, 6> ncalls = {1.e6, 1.e7, 1.e8, 1.e9, 2.e9, 4.e9};
   std::array<int, 7> ndims = {2, 3, 4, 5, 6, 7, 8};
-  std::array<int, 4> chunkSizes = {2048, 1024, 32, 2};
+  std::array<int, 3> chunkSizes = {2048, 1024, 32};
 
-  for (auto ncall : ncalls)
-    for (auto ndim : ndims)
+  for (auto ncall : ncalls) {
+    for (auto ndim : ndims) {
       for (auto chunkSize : chunkSizes) {
-        Reduction(ncall, chunkSize, ndim);
+        Kernel_Params kernel_params(ncall, chunkSize, ndim);
+
+        double* d_result = cuda_malloc<double>(1);
+
+        double* block_reductions = cuda_malloc_managed<double>(
+          static_cast<size_t>(kernel_params.nBlocks));
+        double fgb = 1.1;
+
+        run_block_reduce_sum<<<kernel_params.nBlocks, kernel_params.nThreads>>>(
+          kernel_params.totalNumThreads,
+          d_result,
+          block_reductions);
+        cudaDeviceSynchronize();
+
+        double result = 0.0;
+        cuda_memcpy_to_host(&result, d_result, 1); // newly allocated
+
+        for (uint32_t i = 0; i < kernel_params.nBlocks - 1; i++) {
+          CHECK(block_reductions[i] == Approx(BLOCK_DIM_X * fgb));
+        }
+
+        CHECK(result == Approx(kernel_params.totalNumThreads * fgb));
       }
+    }
+  }
+}
+
+TEST_CASE("misaigned reduction")
+{
+  // do something here
 }
