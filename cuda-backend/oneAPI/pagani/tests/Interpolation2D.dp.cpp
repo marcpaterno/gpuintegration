e2ff75afc8ddb8222336fc63d445ad8147189409
isaki001 <isaki001@odu.edu>
2022-10-09 18:13:04 -0400

    catch header

--- cuda-backend/oneAPI/pagani/tests/Interpolation2D.dp.cpp
+++ cuda-backend/oneAPI/pagani/tests/Interpolation2D.dp.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include <CL/sycl.hpp>
 #include <dpct/dpct.hpp>
-#include "oneAPI/catch2/catch.hpp"
+#include "catch2/catch.hpp"
 #include "oneAPI/pagani/quad/GPUquad/Interp2D.hpp"
 #include "oneAPI/pagani/quad/util/cudaMemoryUtil.h"
 
