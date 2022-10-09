8914522278364cb5a873bf18592dc1d3a513b0ad
isaki001 <isaki001@odu.edu>
2022-10-09 12:04:45 -0400

    resolve conflict

--- cuda/pagani/quad/GPUquad/Sample.cuh
+++ cuda/pagani/quad/GPUquad/Sample.cuh
@@ -518,182 +518,185 @@ namespace quad {
     double& sum,
     double& sq_sum,
     double vol)
-  {
+  {	
+	//random number generation for bin selection
+	gpu::cudaArray<T, NDIM> x_random;
+	constexpr size_t nbins = 100;
+	int ndmx_p1 = nbins+1;
+	__shared__ double xi[(nbins+1)*(NDIM+1)];
+	__shared__ double d[(nbins+1) * (NDIM+1)];
+	double dt[NDIM+1];   
+	const int mxdim_p1 = NDIM + 1;
+	double r[nbins+1];
+	double xin[nbins+1];
+	
+	//size_t FEVAL = CuhreFuncEvalsPerRegion<NDIM>();
+	//size_t num_passes = num_samples/FEVAL;
+	
+	if(threadIdx.x == 0){
+		
+		for (int j = 0; j <= NDIM; j++) {
+			xi[j * ndmx_p1 + 1] = 1.0; // this index is the first for each bin for each dimension
+			
+			for(int bin = 0; bin <= nbins; ++bin){
+				d[bin * (NDIM + 1) + j] = 0.;
+			}
+		}
+		
+		for (int bin = 1; bin <= nbins; bin++)
+		  r[bin] = 1.0;
+	  
+		for (int dim = 1; dim <= NDIM; dim++) {
+		  rebin(1. / nbins, nbins, r, xin, &xi[dim * ndmx_p1]);
+		}
+		
+		for (int dim = 1; dim <= NDIM; dim++){
+			for(int bin=1; bin <= nbins; ++bin){
+					
+				size_t xi_i = (dim)*(nbins+1) + bin;
+				size_t d_i = bin * mxdim_p1 + dim;
+					
+				if(threadIdx.x == 0 && blockIdx.x == 9)
+					printf("xi %i, %i, %i, %i, %e, %e\n", 0, blockIdx.x, dim, bin, xi[xi_i], d[d_i]);
+			}
+		}		
+	}
+		
+	__syncthreads();
+		
+	for(size_t pass = 0; pass < num_passes; ++pass){
+		
+		double local_sq_sum = 0;
+		double local_sum = 0;
+		
+		for(size_t sample = 0; sample < num_samples; ++sample){
+			//use one thread to see what contribution gets marked
+			int bins[NDIM+1];
+			
+			double wgt = 1.;
+			
+			for (int dim = 1; dim <= NDIM; ++dim) {
+				//draw random number
+				const double random = (rand_num_generator)();
+				
+				//select bin with that random number
+				const double probability = 1./static_cast<double>(nbins);
+				const int bin = static_cast<int>(random/probability) + 1;
+				bins[dim] = bin;
+				
+				const double bin_high = xi[(dim)*(nbins+1) + bin];
+				const double bin_low = xi[(dim)*(nbins+1) + bin - 1];
+						
+				//get the true bounds
+				const double region_scaled_b_high = scale_point(bin_high, b[dim-1].lower, b[dim-1].upper);
+				const double region_scaled_b_low = scale_point(bin_low, b[dim-1].lower, b[dim-1].upper);
+				//scale a random point at those bounds
+				
+				double rand_point_in_bin = scale_point((rand_num_generator)(), region_scaled_b_low, region_scaled_b_high);
+				x_random[dim-1] = rand_point_in_bin;
+				wgt *= nbins * (bin_high - bin_low);
+			}
+			
+			double calls = 64. * num_passes * num_samples;
+			double f =  gpu::apply(*d_integrand, x_random) * (*jacobian) * wgt/calls;
+			
+			//the jacobian variable in shared memory is for the global bounds
+			//the volume variable in shared memory is the one about the sub-region
+			
+			/*
+				in mcubes double f = wgt * gpu::apply(*d_integrand, xx);
+				wgt = xjac * (xo * xnd)^ndim where xnd = the # of bins, xo is the length of the bin
+					xjac = (1/calls) * jacobian of global bounds (in this case jacobian would be for region bounds)
+			*/
+			
+			local_sum += f;
+			local_sq_sum +=  f*f;
+			
+			for(int dim = 1; dim <= NDIM; ++dim){
+				atomicAdd(&d[bins[dim] * mxdim_p1 + dim], f * f);
+			}
+		}
+		
+		local_sq_sum += sqrt(local_sq_sum * num_samples);
+		local_sq_sum += (local_sq_sum - local_sum) * (local_sq_sum + local_sum);
+		
+		if(threadIdx.x == 0 && blockIdx.x == 0)
+			printf("per-pass %e / %e / %e\n", local_sum, local_sum*local_sum, local_sq_sum);
+		
+		if(local_sq_sum <= 0.)
+			local_sq_sum = 1.e-100;
+		
+		sum += local_sum;
+		sq_sum += local_sq_sum;
+		
+		double xo = 0.;
+		double xn = 0.;
+		double rc = 0.;
+		
+		__syncthreads();
+		
+		if(threadIdx.x == 0){
+									
+		for (int dim = 1; dim <= NDIM; dim++){
+			for(int bin = 1; bin <= nbins; ++bin){
 
-    // random number generation for bin selection
-    gpu::cudaArray<T, NDIM> x_random;
-    constexpr size_t nbins = 10;
-    int ndmx_p1 = nbins + 1;
-    __shared__ double xi[(nbins + 1) * (NDIM + 1)];
-    __shared__ double d[(nbins + 1) * (NDIM + 1)];
-    double dt[NDIM + 1];
-    const int mxdim_p1 = NDIM + 1;
-    double r[nbins + 1];
-    double xin[nbins + 1];
+				size_t xi_i = (dim)*(nbins+1) + bin;
+				size_t d_i = bin * mxdim_p1 + dim;
+					
+				if(threadIdx.x == 0 && blockIdx.x == 9)
+					printf("xi %lu, %i, %i, %i, %e, %e\n", pass + 1, blockIdx.x, dim, bin, xi[xi_i], d[d_i]);
+			}
+		}		
+			
+			
+		for (int j = 1; j <= NDIM; j++) {
+				
+			//avg contribution from first two bins 
+			xo = d[1 * mxdim_p1 + j]; // bin 1 of dim j
+			xn = d[2 * mxdim_p1 + j]; // bin 2 of dim j
+			d[1 * mxdim_p1 + j] = (xo + xn) / 2.0;
+				
+			dt[j] = d[1 * mxdim_p1 + j]; // set dt sum to contribution of bin 1
+				
+			for (int i = 2; i < nbins; i++) {
+					
+				rc = xo + xn;//here xn is contr of previous bin
+				xo = xn;
+					
+				xn = d[(i + 1) * mxdim_p1 + j];//here takes contr of next bin
+				d[i * mxdim_p1 + j] = (rc + xn) / 3.0; //avg of three bins
+				dt[j] += d[i * mxdim_p1 + j]; //running sum of all contributions
+			}
 
-    // size_t FEVAL = CuhreFuncEvalsPerRegion<NDIM>();
-    // size_t num_passes = num_samples/FEVAL;
-
-    if (threadIdx.x == 0) {
-      for (int j = 0; j <= NDIM; j++) {
-        xi[j * ndmx_p1 + 1] =
-          1.0; // this index is the first for each bin for each dimension
-        for (int bin = 0; bin <= nbins; ++bin) {
-          d[bin * (NDIM + 1) + j] = 0.;
-          // printf("d[%i]:%f\n",bin * (NDIM + 1) + j, d[bin * (NDIM + 1) + j]);
-        }
-      }
-
-      for (int i = 1; i <= nbins; i++)
-        r[i] = 1.0;
-      for (int j = 1; j <= NDIM; j++) {
-        rebin(1. / nbins, nbins, r, xin, &xi[j * ndmx_p1]);
-      }
-
-      /*for(int i=0; i < (nbins+1)*(NDIM+1); ++i){
-              if(threadIdx.x == 0 && blockIdx.x == 1)
-                      printf("xi[%i]:%f\n", i, xi[i]);
-      }*/
-    }
-
-    __syncthreads();
-
-    for (size_t pass = 0; pass < num_passes; ++pass) {
-
-      for (size_t sample = 0; sample < num_samples; ++sample) {
-        // use one thread to see what contribution gets marked
-        int bins[NDIM + 1];
-
-        for (int dim = 1; dim <= NDIM; ++dim) {
-          // draw random number
-          const double random = (rand_num_generator)();
-
-          // select bin with that random number
-          const double probability = 1. / static_cast<double>(nbins);
-          const int bin = static_cast<int>(random / probability) + 1;
-          bins[dim] = bin;
-
-          const double bin_high = xi[(dim) * (nbins + 1) + bin];
-          const double bin_low = xi[(dim) * (nbins + 1) + bin - 1];
-
-          // if(threadIdx.x == 0 && blockIdx.x == 0)
-          //	printf("dim %i bin %i bounds:%f,%f\n", dim, bin, bin_low,
-          // bin_high);
-
-          // get the true bounds
-          const double region_scaled_b_high =
-            scale_point(bin_high, b[dim - 1].lower, b[dim - 1].upper);
-          const double region_scaled_b_low =
-            scale_point(bin_low, b[dim - 1].lower, b[dim - 1].upper);
-
-          // scale a random point at those bounds
-
-          double rand_point_in_bin = scale_point(
-            (rand_num_generator)(), region_scaled_b_low, region_scaled_b_high);
-
-          /*if(threadIdx.x == 0 && blockIdx.x == 15)
-                  printf("sample %lu bin %i dim %i scaled bin bounds:%f,%f
-             point:%e\n", sample, bin, dim, region_scaled_b_low,
-             region_scaled_b_high, rand_point_in_bin);*/
-          x_random[dim - 1] = rand_point_in_bin;
-        }
-
-        double f = gpu::apply(*d_integrand, x_random) * (*jacobian);
-
-        /*if(f != 0.  && blockIdx.x == 0){
-                printf("\t\t feval tid:%i f:%e, bins:(%i,%i) contr
-        indices:(%i,%i) %f, %f\n", threadIdx.x, f, bins[1], bins[2], bins[1] *
-        (NDIM+1) + 1, bins[2] * (NDIM+1) + 2, x_random[0], x_random[1]);
-        }*/
-
-        sum += f;
-        sq_sum += f * f;
-
-        for (int dim = 1; dim <= NDIM; ++dim) {
-          atomicAdd(&d[bins[dim] * (NDIM + 1) + dim], f * f);
-          /*
-          if(f != 0.)
-                  printf("tid %i dim:%i bin:%i adding %e (f:%e) to index
-          d[%i]\n", threadIdx.x, dim, bins[dim], pass_sq_sum, pass_sum,
-          bins[dim] * (NDIM+1) + dim);*/
-        }
-      }
-
-      double xo = 0.;
-      double xn = 0.;
-      double rc = 0.;
-
-      __syncthreads();
-
-      if (threadIdx.x == 0) {
-
-        /*if(blockIdx.x == 15){
-                printf("---------------------------------\n");
-                for(int dim=1; dim <= NDIM; ++dim){
-                        for(int bin = 1; bin <= nbins; ++bin){
-                        printf("dim :%i bin %i contribution:%e bounds:%f-%f\n",
-        dim, bin, d[(bin)*(NDIM+1) + dim], scale_point(xi[(dim)*(nbins+1) + bin
-        -1], b[dim-1].lower, b[dim-1].upper), scale_point(xi[(dim)*(nbins+1) +
-        bin], b[dim-1].lower, b[dim-1].upper));
-                        }
-                        printf("---------------------------------\n");
-                }
-                printf("========================\n");
-        }*/
-
-        for (int j = 1; j <= NDIM; j++) {
-          // avg contribution from first two bins
-          xo = d[1 * mxdim_p1 + j]; // bin 1 of dim j
-          xn = d[2 * mxdim_p1 + j]; // bin 2 of dim j
-          d[1 * mxdim_p1 + j] = (xo + xn) / 2.0;
-
-          dt[j] = d[1 * mxdim_p1 + j]; // set dt sum to contribution of bin 1
-
-          for (int i = 2; i < nbins; i++) {
-
-            rc = xo + xn; // here xn is contr of previous bin
-            xo = xn;
-
-            xn = d[(i + 1) * mxdim_p1 + j]; // here takes contr of next bin
-            d[i * mxdim_p1 + j] = (rc + xn) / 3.0; // avg of three bins
-            dt[j] += d[i * mxdim_p1 + j]; // running sum of all contributions
-          }
-
-          d[nbins * mxdim_p1 + j] = (xo + xn) / 2.0; // do bin nd last
-          dt[j] += d[nbins * mxdim_p1 + j];
-        }
-
-        for (int j = 1; j <= NDIM; j++) {
-          if (dt[j] > 0.0) { // enter if there is any contribution only
-            rc = 0.0;
-            for (int i = 1; i <= nbins; i++) {
-              r[i] = pow((1.0 - d[i * mxdim_p1 + j] / dt[j]) /
-                           (log(dt[j]) - log(d[i * mxdim_p1 + j])),
-                         1.5);
-              rc += r[i];
-            }
-            rebin(rc / nbins,
-                  nbins,
-                  r,
-                  xin,
-                  &xi[j * ndmx_p1]); // first bin of each dimension is at a diff
-                                     // index
-          }
-        }
-
-        for (int j = 1; j <= NDIM; j++) {
-          for (int i = 1; i <= nbins; i++)
-            d[i * mxdim_p1 + j] = 0.0;
-        }
-
-        /*
-        for(int i=0; i < (nbins+1)*(NDIM+1); ++i){
-                if(threadIdx.x == 0 && blockIdx.x == 1)
-                printf("xi[%i]:%f\n", i, xi[i]);
-        }*/
-      }
-      __syncthreads();
-    }
+			d[nbins * mxdim_p1 + j] = (xo + xn) / 2.0; // do bin nd last
+			dt[j] += d[nbins * mxdim_p1 + j];
+		}
+			
+		for (int j = 1; j <= NDIM; j++) {
+			if (dt[j] > 0.0) { // enter if there is any contribution only
+				rc = 0.0;
+				for (int i = 1; i <= nbins; i++) {
+					r[i] = pow((1.0 - d[i * mxdim_p1 + j] / dt[j]) /
+						(log(dt[j]) - log(d[i * mxdim_p1 + j])), .5);
+					rc += r[i];
+				}
+				
+				
+				rebin( rc / nbins, nbins, r, xin, &xi[j * ndmx_p1]); // first bin of each dimension is at a diff index
+			}
+		}
+			
+		for (int j = 1; j <= NDIM; j++) {
+			for (int i = 1; i <= nbins; i++)
+				d[i * mxdim_p1 + j] = 0.0;
+		}
+			
+			
+			
+		}
+		__syncthreads();
+		
+	}
   }
 
   template <typename IntegT,
@@ -717,15 +720,6 @@ namespace quad {
     pagani::Curand_generator rand_num_generator(seed_init);
     Region<NDIM>* const region = (Region<NDIM>*)&sRegionPool[0];
     __shared__ T sdata[blockdim];
-
-    /*if(threadIdx.x == 0)
-            printf("region %i bounds: (%f - %f) (%f - %f)  (%f - %f)  (%f - %f)
-       (%f - %f)\n", blockIdx.x, region->bounds[0].lower,
-       region->bounds[0].upper, region->bounds[1].lower,
-       region->bounds[1].upper, region->bounds[2].lower,
-       region->bounds[2].upper, region->bounds[3].lower,
-       region->bounds[3].upper, region->bounds[4].lower,
-       region->bounds[4].upper);*/
 
     int perm = 0;
     constexpr int offset = 2 * NDIM;
@@ -846,58 +840,58 @@ namespace quad {
                           errcoeff[0] * sum[2] <= sum[3]) ?
                            errcoeff[1] * sum[1] :
                            errcoeff[2] * max(max(sum[1], sum[2]), sum[3]));
-    }
-    __syncthreads();
-
-    size_t num_samples = 100000;
-    size_t num_passes = 2;
-    double ran_sum = 0.;
-    double sq_sum = 0.;
-
-    Vegas_assisted_computePermutation<IntegT, T, NDIM>(d_integrand,
-                                                       num_samples,
-                                                       num_passes,
-                                                       region->bounds,
-                                                       sBound,
-                                                       range,
-                                                       jacobian,
-                                                       rand_num_generator,
-                                                       ran_sum,
-                                                       sq_sum,
-                                                       vol[0]);
-
-    sq_sum = sqrt(sq_sum * num_samples);
-    sq_sum = (sq_sum - ran_sum) * (sq_sum + ran_sum);
-
-    if (sq_sum <= 0.)
-      sq_sum = 1.e-100;
-
-    __syncthreads();
-    ran_sum = blockReduceSum(ran_sum);
-    sq_sum = blockReduceSum(sq_sum);
-    __syncthreads();
-
-    if (threadIdx.x == 0) {
-      Result* r = &region->result;
-      double mean =
-        ran_sum / static_cast<double>(64 * num_samples * num_passes);
-      double var = sq_sum / static_cast<double>(64 * num_passes * num_samples) -
-                   mean * mean;
-      // printf("sum:%e (squared:%e) sq_sum:%e\n", ran_sum, ran_sum*ran_sum,
-      // sq_sum);
-      //printf("region %ul mcubes:%e +- %e (sum:%e) nsamples:%ul\n",
-      //       blockIdx.x,
-      //       vol[0] * mean,
-      //       var,
-      //       ran_sum,
-      //       num_samples * num_passes * 64);
-      // printf("region %i pagani:%e +- %e mcubes:%e +- %e (ran_sum:%e)\n",
-      // blockIdx.x, r->avg, r->err, vol[0]*mean, var, ran_sum);
-      r->avg = vol[0] * mean;
-      r->err = sqrt(var);
-    }
-  }
-
+	}
+	__syncthreads();
+	
+	size_t num_samples = 100;
+	size_t num_passes = 10;
+	double ran_sum = 0.;
+	double sq_sum = 0.;
+	
+	
+	Vegas_assisted_computePermutation<IntegT, T, NDIM>(d_integrand,
+					 num_samples,
+					 num_passes,
+                     region->bounds,
+                     sBound,
+                     range,
+                     jacobian,
+					 rand_num_generator,
+					 ran_sum,
+					 sq_sum,
+					 vol[0]);
+	
+	__syncthreads();
+	ran_sum = blockReduceSum(ran_sum);
+	sq_sum = blockReduceSum(sq_sum);
+	__syncthreads();
+	
+	if(threadIdx.x == 0){
+		Result* r = &region->result;	
+		//double mean = ran_sum / static_cast<double>(64* num_samples * num_passes);
+		//double var = sq_sum / static_cast<double>(64*num_passes * num_samples) - mean* mean;
+		//printf("region %i mcubes:%e +- %e (sum:%e) nsamples:%i\n", blockIdx.x, vol[0]*mean, var, ran_sum, num_samples*num_passes*64);
+		
+		double dxg = 1.0 / (num_passes*num_samples*64);
+		double dv2g, i;
+		double calls = num_passes*num_samples*64;
+		for (dv2g = 1, i = 1; i <= NDIM; i++)
+			dv2g *= dxg;
+		dv2g = (calls * dv2g * calls * dv2g) / num_samples / num_samples / (num_samples - 1.0);
+		
+		
+		printf("region %i pagani:%e +- %e mcubes:%e +- %e (ran_sum:%e)\n", 
+			blockIdx.x, 
+			r->avg, r->err, 
+			ran_sum, sqrt(sq_sum * dv2g),
+			//vol[0]*mean, sqrt(sq_sum * dv2g), 
+			ran_sum);
+		
+		r->avg = vol[0] * ran_sum;
+		r->err = sqrt(sq_sum * dv2g);
+	}
+	
+  }	
 }
 
 #endif
