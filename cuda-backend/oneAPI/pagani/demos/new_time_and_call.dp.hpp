1afed384748851f25f726887599015fdd10ae5e3
isaki001 <isaki001@odu.edu>
2022-10-09 20:13:47 -0400

    resolve conflict

--- cuda-backend/oneAPI/pagani/demos/new_time_and_call.dp.hpp
+++ cuda-backend/oneAPI/pagani/demos/new_time_and_call.dp.hpp
@@ -13,45 +13,36 @@
 
 template <typename F, int ndim>
 void
-call_cubature_rules(F integrand, quad::Volume<double, ndim>& vol)
-{
-
-  F* d_integrand = make_gpu_integrand<F>(integrand);
-  size_t partitions_per_axis = 2;
-  if (ndim < 5)
-    partitions_per_axis = 4;
-  else if (ndim <= 10)
-    partitions_per_axis = 2;
-  else
-    partitions_per_axis = 1;
-  partitions_per_axis = 8;
-  Sub_regions<ndim> sub_regions(partitions_per_axis);
-  size_t num_regions = sub_regions.size;
-  std::cout << "Initial regions:" << sub_regions.size << std::endl;
-  Region_characteristics<ndim> characteristics(sub_regions.size);
-  Region_estimates<ndim> estimates(sub_regions.size);
-  Cubature_rules<ndim> rules;
-  rules.set_device_volume(vol.lows, vol.highs);
-  int iteration = 0;
-  bool compute_relerr_error_reduction = false;
-  cuhreResult<double> iter = rules.template apply_cubature_integration_rules<F>(
-    d_integrand,
-    iteration,
-    &sub_regions,
-    &estimates,
-    &characteristics,
-    compute_relerr_error_reduction);
-
-  double estimate =
-    reduction<double>(estimates.integral_estimates, num_regions);
-  double errorest = reduction<double>(estimates.error_estimates, num_regions);
-
-  std::cout << std::setprecision(15) << std::scientific << iter.estimate << ","
-            << iter.errorest << std::endl;
-  dpct::device_ext& dev_ct1 = dpct::get_current_device();
-  sycl::queue& q_ct1 = dev_ct1.default_queue();
-  sycl::free(d_integrand, q_ct1);
-}
+call_cubature_rules(F integrand, quad::Volume<double, ndim>&  vol){
+	
+	F* d_integrand = make_gpu_integrand<F>(integrand);
+	size_t partitions_per_axis = 2;   
+	if(ndim < 5)
+		partitions_per_axis = 4;
+	else if(ndim <= 10)
+		partitions_per_axis = 2;
+	else
+		partitions_per_axis = 1;
+	partitions_per_axis = 8;
+	Sub_regions<ndim> sub_regions(partitions_per_axis);
+	size_t num_regions = sub_regions.size;
+	std::cout<<"Initial regions:"<<sub_regions.size << std::endl;
+    Region_characteristics<ndim> characteristics(sub_regions.size);
+    Region_estimates<ndim> estimates(sub_regions.size);
+	Cubature_rules<ndim> rules;
+    rules.set_device_volume(vol.lows, vol.highs);
+	int iteration = 0;
+	bool compute_relerr_error_reduction = false;
+	cuhreResult<double> iter = rules.template apply_cubature_integration_rules<F>(d_integrand, iteration, &sub_regions, &estimates, &characteristics, compute_relerr_error_reduction);
+	
+	double estimate = reduction<double>(estimates.integral_estimates, num_regions);
+	double errorest = reduction<double>(estimates.error_estimates, num_regions);
+	
+	std::cout << std::setprecision(15) << std::scientific << iter.estimate << "," << iter.errorest << std::endl;
+	dpct::device_ext& dev_ct1 = dpct::get_current_device();
+    sycl::queue& q_ct1 = dev_ct1.default_queue();
+	sycl::free(d_integrand, q_ct1);
+}	
 
 /*
     we are not keeping track of nFinished regions
@@ -83,17 +74,18 @@ clean_time_and_call(std::string id,
   Workspace<ndim, use_custom> workspace;
   quad::Volume<double, ndim> vol;
 
-  for (int i = 0; i < 2; i++) {
-    auto const t0 = std::chrono::high_resolution_clock::now();
-    size_t partitions_per_axis = 2;
-    if (ndim < 5)
-      partitions_per_axis = 4;
-    else if (ndim <= 10)
-      partitions_per_axis = 2;
-    else
-      partitions_per_axis = 1;
-    Sub_regions<ndim> sub_regions(partitions_per_axis);
-    // std::cout<<"initial num_regions:"<<sub_regions.size<<std::endl;
+  
+  for(int i=0; i < 2; i++){
+	auto const t0 = std::chrono::high_resolution_clock::now();
+	size_t partitions_per_axis = 2;   
+	if(ndim < 5)
+		partitions_per_axis = 4;
+	else if(ndim <= 10)
+		partitions_per_axis = 2;
+	else
+		partitions_per_axis = 1;
+	Sub_regions<ndim> sub_regions(partitions_per_axis);
+
     constexpr bool collect_iters = false;
     constexpr bool collect_sub_regions = false;
     constexpr bool predict_split = false;
