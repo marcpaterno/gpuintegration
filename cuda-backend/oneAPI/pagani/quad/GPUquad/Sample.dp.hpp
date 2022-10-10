e427100b4220845ffe9569b60ac8d000a017148c
isaki001 <isaki001@odu.edu>
2022-10-10 11:14:35 -0400

    oneapi profiling wip

--- cuda-backend/oneAPI/pagani/quad/GPUquad/Sample.dp.hpp
+++ cuda-backend/oneAPI/pagani/quad/GPUquad/Sample.dp.hpp
@@ -10,7 +10,6 @@
 #include "oneAPI/pagani/quad/util/cudaUtil.h"
 #include "oneAPI/pagani/quad/GPUquad/Func_Eval.hpp"
 
-
 namespace quad {
   template <typename T>
   T
@@ -85,10 +84,10 @@ namespace quad {
 
   template <typename T>
   T
-  blockReduceSum(T val, sycl::nd_item<1> item_ct1, T *shared)
+  blockReduceSum(T val, sycl::nd_item<1> item_ct1, T* shared)
   {
 
-     // why was this set to 8?
+    // why was this set to 8?
     int lane = item_ct1.get_local_id(0) % 32; // 32 is for warp size
     int wid = item_ct1.get_local_id(0) >> 5 /* threadIdx.x / 32  */;
 
@@ -108,21 +107,21 @@ namespace quad {
       (item_ct1.get_local_id(0) < (item_ct1.get_local_range().get(0) >> 5)) ?
         shared[lane] :
         0;
-      
+
     item_ct1.barrier();
-    
+
     if (wid == 0)
       val = warpReduceSum(val, item_ct1); // Final reduce within first warp
 
     return val;
   }
-	
-	  template <typename T>
+
+  template <typename T>
   T
-  blockReduceSum(T val, sycl::nd_item<3> item_ct1, T *shared)
+  blockReduceSum(T val, sycl::nd_item<3> item_ct1, T* shared)
   {
 
-     // why was this set to 8?
+    // why was this set to 8?
     int lane = item_ct1.get_local_id(2) % 32; // 32 is for warp size
     int wid = item_ct1.get_local_id(2) >> 5 /* threadIdx.x / 32  */;
 
@@ -142,16 +141,15 @@ namespace quad {
       (item_ct1.get_local_id(2) < (item_ct1.get_local_range().get(0) >> 5)) ?
         shared[lane] :
         0;
-      
+
     item_ct1.barrier();
-    
+
     if (wid == 0)
       val = warpReduceSum(val, item_ct1); // Final reduce within first warp
 
     return val;
   }
-	
-	
+
   template <typename T>
   T
   computeReduce(T sum, T* sdata, sycl::nd_item<3> item_ct1)
@@ -181,26 +179,23 @@ namespace quad {
     }
     return sdata[0];
   }
-    
+
   template <typename IntegT, typename T, int NDIM, int debug = 0>
   void
   computePermutation(IntegT* d_integrand,
                      int pIndex,
                      Bounds* b,
                      GlobalBounds sBound[],
-                     //T* g,
-                     //gpu::cudaArray<T, NDIM>& x,
                      T* sum,
-                     Structures<double> constMem,
+                     Structures<double>& constMem,
                      T range[],
                      T* jacobian,
                      double* generators,
-                     //int FEVAL,
                      T* sdata,
                      sycl::nd_item<1> item_ct1,
                      quad::Func_Evals<NDIM>& fevals)
   {
-        
+
     gpu::cudaArray<T, NDIM> x;
     for (int dim = 0; dim < NDIM; ++dim) {
       /*
@@ -213,28 +208,24 @@ namespace quad {
                                             (.5 - generator) * b[dim].upper) *
                                              range[dim];
     }
-	
+
     const T fun = gpu::apply(*d_integrand, x) * (*jacobian);
     sdata[item_ct1.get_local_id(0)] = fun; // target for reduction
-        /*
-        DPCT1026:16: The call to __ldg was removed because there is no
-        correspoinding API in DPC++.
-        */
+                                           /*
+                                           DPCT1026:16: The call to __ldg was removed because there is no
+                                           correspoinding API in DPC++.
+                                           */
     const int gIndex = constMem._gpuGenPermGIndex[pIndex];
-      
-    if constexpr(debug >= 2){
-        fevals[item_ct1.get_group(0) * CuhreFuncEvalsPerRegion<NDIM>() + pIndex].store(x, sBound, b);
-        fevals[item_ct1.get_group(0) * CuhreFuncEvalsPerRegion<NDIM>() + pIndex].store(gpu::apply(*d_integrand, x), pIndex);
+
+    if constexpr (debug >= 2) {
+      fevals[item_ct1.get_group(0) * CuhreFuncEvalsPerRegion<NDIM>() + pIndex]
+        .store(x, sBound, b);
+      fevals[item_ct1.get_group(0) * CuhreFuncEvalsPerRegion<NDIM>() + pIndex]
+        .store(gpu::apply(*d_integrand, x), pIndex);
     }
-      
-      
-      
+
 #pragma unroll 5
     for (int rul = 0; rul < NRULES; ++rul) {
-      /*
-      DPCT1026:17: The call to __ldg was removed because there is no
-      correspoinding API in DPC++.
-      */
       sum[rul] += fun * constMem._cRuleWt[gIndex * NRULES + rul];
     }
   }
@@ -243,7 +234,7 @@ namespace quad {
   template <typename IntegT, typename T, int NDIM, int blockdim, int debug = 0>
   void
   SampleRegionBlock(IntegT* d_integrand,
-                    Structures<double> constMem,
+                    Structures<double>& constMem,
                     Region<NDIM> sRegionPool[],
                     GlobalBounds sBound[],
                     T* vol,
@@ -252,17 +243,17 @@ namespace quad {
                     T* jacobian,
                     double* generators,
                     sycl::nd_item<1> item_ct1,
-                    T *shared,
-                    T *sdata,
+                    T* shared,
+                    T* sdata,
                     quad::Func_Evals<NDIM>& fevals)
   {
     Region<NDIM>* const region = (Region<NDIM>*)&sRegionPool[0];
     int perm = 0;
     constexpr int offset = 2 * NDIM;
-    
+
     T sum[NRULES];
     Zap(sum);
-    
+
     // Compute first set of permutation outside for loop to extract the Function
     // values for the permutation used to compute
     // fourth dimension
@@ -270,31 +261,31 @@ namespace quad {
     constexpr int FEVAL = CuhreFuncEvalsPerRegion<NDIM>();
     if (pIndex < FEVAL) {
       computePermutation<IntegT, T, NDIM, debug>(d_integrand,
-                                          pIndex,
-                                          region->bounds,
-                                          sBound,
-                                          sum,
-                                          constMem,
-                                          range,
-                                          jacobian,
-                                          generators,
-                                          sdata,
-                                          item_ct1,
-                                          fevals);
+                                                 pIndex,
+                                                 region->bounds,
+                                                 sBound,
+                                                 sum,
+                                                 constMem,
+                                                 range,
+                                                 jacobian,
+                                                 generators,
+                                                 sdata,
+                                                 item_ct1,
+                                                 fevals);
     }
-	
+
     /*
     DPCT1065:23: Consider replacing sycl::nd_item::barrier() with
     sycl::nd_item::barrier(sycl::access::fence_space::local_space) for better
     performance if there is no access to global memory.
     */
     item_ct1.barrier();
-	
+
     if (item_ct1.get_local_id(0) == 0) {
-          /*
-          DPCT1026:25: The call to __ldg was removed because there is no
-          correspoinding API in DPC++.
-          */
+      /*
+      DPCT1026:25: The call to __ldg was removed because there is no
+      correspoinding API in DPC++.
+      */
       const T ratio = Sq(constMem._gpuG[2 * NDIM] / constMem._gpuG[1 * NDIM]);
       T* f = &sdata[0];
       Result* r = &region->result;
@@ -327,20 +318,17 @@ namespace quad {
     for (perm = 1; perm < FEVAL / blockdim; ++perm) {
       int pIndex = perm * blockdim + item_ct1.get_local_id(0);
       computePermutation<IntegT, T, NDIM, debug>(d_integrand,
-                                          pIndex,
-                                          region->bounds,
-                                          sBound,
-                                          // g,
-                                          // x,
-                                          sum,
-                                          constMem,
-                                          range,
-                                          jacobian,
-                                          generators,
-                                          // FEVAL,
-                                          sdata,
-                                          item_ct1, 
-                                          fevals);
+                                                 pIndex,
+                                                 region->bounds,
+                                                 sBound,
+                                                 sum,
+                                                 constMem,
+                                                 range,
+                                                 jacobian,
+                                                 generators,
+                                                 sdata,
+                                                 item_ct1,
+                                                 fevals);
     }
     //__syncthreads();
     // Balance permutations
@@ -348,48 +336,49 @@ namespace quad {
     if (pIndex < FEVAL) {
       int pIndex = perm * blockdim + item_ct1.get_local_id(0);
       computePermutation<IntegT, T, NDIM, debug>(d_integrand,
-                                          pIndex,
-                                          region->bounds,
-                                          sBound,
-                                          sum,
-                                          constMem,
-                                          range,
-                                          jacobian,
-                                          generators,
-                                          sdata,
-                                          item_ct1,
-                                          fevals);
+                                                 pIndex,
+                                                 region->bounds,
+                                                 sBound,
+                                                 sum,
+                                                 constMem,
+                                                 range,
+                                                 jacobian,
+                                                 generators,
+                                                 sdata,
+                                                 item_ct1,
+                                                 fevals);
     }
-	
-	item_ct1.barrier();
+
+    item_ct1.barrier();
     for (int i = 0; i < NRULES; ++i) {
       sum[i] = blockReduceSum(sum[i], item_ct1, shared);
-	  //sum[i] = sycl::reduce_over_group(item_ct1.get_group(), sum[i], sycl::plus<>());
+      // sum[i] = sycl::reduce_over_group(item_ct1.get_group(), sum[i],
+      // sycl::plus<>());
       //__syncthreads();
     }
 
     if (item_ct1.get_local_id(0) == 0) {
       Result* r = &region->result;
-	  
-	  #pragma unroll 4
+
+#pragma unroll 3
       for (int rul = 1; rul < NRULES - 1; ++rul) {
         T maxerr = 0.;
-		
-		constexpr int NSETS = 9;
-		#pragma unroll 9
+
+        constexpr int NSETS = 9;
+#pragma unroll 9
         for (int s = 0; s < NSETS; ++s) {
-          maxerr = sycl::max(
-            maxerr,
-            (double)(sycl::fabs(sum[rul + 1] +
-                                constMem._GPUScale[s * NRULES + rul] *
-                                  sum[rul]) *
-                     constMem._GPUNorm[s * NRULES + rul]));
+          maxerr =
+            sycl::max(maxerr,
+                      (double)(sycl::fabs(sum[rul + 1] +
+                                          constMem._GPUScale[s * NRULES + rul] *
+                                            sum[rul]) *
+                               constMem._GPUNorm[s * NRULES + rul]));
         }
         sum[rul] = maxerr;
       }
-      
+
       r->avg = (*vol) * sum[0];
-	  const double errcoeff[3] = {5., 1., 5.};
+      const double errcoeff[3] = {5., 1., 5.};
       r->err =
         (*vol) *
         ((errcoeff[0] * sum[1] <= sum[2] && errcoeff[0] * sum[2] <= sum[3]) ?
--- cuda/pagani/demos/new_interface_Genz4_6D.cu
+++ cuda/pagani/demos/new_interface_Genz4_6D.cu
@@ -1,53 +1,53 @@
 #include <iostream>
 #include "cuda/pagani/demos/new_time_and_call.cuh"
 
-  class GENZ_4_6D {
-  public:
-    __device__ __host__ double
-    operator()(double x, double y, double z, double w, double v, double b)
-    {
-      // double alpha = 25.;
-      double beta = .5;
-      return exp(
-       -1.0 * (pow(25, 2) * pow(x - beta, 2) + pow(25, 2) * pow(y - beta, 2) +
-                pow(25, 2) * pow(z - beta, 2) + pow(25, 2) * pow(w - beta, 2) +
-                pow(25, 2) * pow(v - beta, 2) + pow(25, 2) * pow(b - beta, 2)));
-    }
-  };
+class GENZ_4_6D {
+public:
+  __device__ __host__ double
+  operator()(double x, double y, double z, double w, double v, double b)
+  {
+    // double alpha = 25.;
+    double beta = .5;
+    return exp(-1.0 * pow(25, 2) *
+               (pow(x - beta, 2) + pow(y - beta, 2) + pow(z - beta, 2) +
+                pow(w - beta, 2) + pow(v - beta, 2) + pow(b - beta, 2)));
+  }
+};
 
-int main(){
-    
-    double epsrel = 1.0e-3;
-    double const epsrel_min = 1.0240000000000002e-10;
-    constexpr int ndim = 6;
-    GENZ_4_6D integrand;
-    double true_value = 1.79132603674879e-06;
-	quad::Volume<double, ndim> vol;
-	
-	for(int i=0; i < 10; ++i)
-		call_cubature_rules<GENZ_4_6D, ndim>(integrand, vol);
+int
+main()
+{
 
-    /*while (clean_time_and_call<GENZ_4_5D, double, ndim, false>("f4",
-                                           integrand,
-                                           epsrel,
-                                           true_value,
-                                           "gpucuhre",
-                                           std::cout,
-										   vol) == true &&
-         epsrel >= epsrel_min) {
-			epsrel /= 5.0;
-	}*/
-	
-	/*epsrel = 1.0e-3;
-	while (clean_time_and_call<GENZ_4_5D, double, ndim, true>("f4",
-                                           integrand,
-                                           epsrel,
-                                           true_value,
-                                           "gpucuhre",
-                                           std::cout) == true &&
-         epsrel >= epsrel_min) {
-			epsrel /= 5.0;
-	}*/
-    return 0;
+  double epsrel = 1.0e-3;
+  double const epsrel_min = 1.0240000000000002e-10;
+  constexpr int ndim = 6;
+  GENZ_4_6D integrand;
+  double true_value = 1.79132603674879e-06;
+  quad::Volume<double, ndim> vol;
+
+  // for(int i=0; i < 10; ++i)
+  call_cubature_rules<GENZ_4_6D, ndim>(integrand, vol);
+
+  /*while (clean_time_and_call<GENZ_4_5D, double, ndim, false>("f4",
+                                         integrand,
+                                         epsrel,
+                                         true_value,
+                                         "gpucuhre",
+                                         std::cout,
+                                                                                 vol) == true &&
+       epsrel >= epsrel_min) {
+                      epsrel /= 5.0;
+      }*/
+
+  /*epsrel = 1.0e-3;
+  while (clean_time_and_call<GENZ_4_5D, double, ndim, true>("f4",
+                                     integrand,
+                                     epsrel,
+                                     true_value,
+                                     "gpucuhre",
+                                     std::cout) == true &&
+   epsrel >= epsrel_min) {
+                  epsrel /= 5.0;
+  }*/
+  return 0;
 }
-
