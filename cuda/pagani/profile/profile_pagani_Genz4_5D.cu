77a14506bb31a614ebfab5044f0d2881bb5e348f
isaki001 <isaki001@odu.edu>
2022-11-11 10:02:00 -0500

    delete comments

--- cuda/pagani/profile/profile_pagani_Genz4_5D.cu
+++ cuda/pagani/profile/profile_pagani_Genz4_5D.cu
@@ -7,40 +7,6 @@ class GENZ_4_5D {
     operator()(double x, double y, double z, double w, double v)
     {
 		double beta = .5;
-		
-		/*return  exp(-1.*(pow(25.,2.) * pow(x - beta, 2.) + 
-				pow(25.,2.) * pow(y - beta, 2.) +
-                pow(25.,2.) * pow(z - beta, 2.) + 
-				pow(25.,2.) * pow(w - beta, 2.) +
-                pow(25.,2.) * pow(v - beta, 2.))); 
-				
-				
-		  
-		 
-		 
-		return  (pow(25.,2) * pow(x - beta, 2.) + 
-				pow(25.,2) * pow(y - beta, 2.) +
-                pow(25.,2) * pow(z - beta, 2.) + 
-				pow(25.,2) * pow(w - beta, 2.) +
-                pow(25.,2) * pow(v - beta, 2.));  */
-		 
-		 
-		/*return  25.*25. * pow(x - beta, 2.) + 
-				25.*25. * pow(y - beta, 2.) +
-                25.*25. * pow(z - beta, 2.) + 
-				25.*25. * pow(w - beta, 2.) +
-                25.*25. * pow(v - beta, 2.); 
-		 
-		 
-		return  25.*25. * (x - beta)*(x - beta) + 
-				25.*25. * (y - beta)*(y - beta) +
-                25.*25. * (z - beta)*(z - beta) + 
-				25.*25. * (w - beta)*(w - beta) +
-                25.*25. * (v - beta)*(v - beta);
-		*/
-	   
-      // double alpha = 25.;
-
       return exp(
         -1.0 * (pow(25., 2.) * pow(x - beta, 2.) + 
 				pow(25., 2.) * pow(y - beta, 2.) +
