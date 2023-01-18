903f9cdae1c4016d538e31e30b0c90c2cf37f7a6
isaki001 <isaki001@odu.edu>
2023-01-18 16:33:33 -0500

    add testing of genz integrals on epsrel:1e-3

--- test/cuda/pagani/Easy_Integrals.cu
+++ test/cuda/pagani/Easy_Integrals.cu
@@ -18,7 +18,7 @@
 #include "cuda/pagani/quad/GPUquad/PaganiUtils.cuh"
 #include "cuda/pagani/quad/GPUquad/Workspace.cuh"
 #include "cuda/pagani/quad/util/Volume.cuh"
-
+#include "cuda/integrands.cuh"
 #include "common/integration_result.hh"
 
 using numint::integration_result;
@@ -56,9 +56,10 @@ TEST_CASE("Constant Positive Value Function")
 {
   constexpr int ndim = 2;
   PTest integrand;
+  constexpr bool use_custom = false;
+
   double epsrel = 1.0e-3;
   double epsabs = 1.0e-12;
-  constexpr bool use_custom = false;
   Workspace<double, ndim, use_custom> pagani;
   quad::Volume<double, ndim> vol;
 
@@ -71,3 +72,79 @@ TEST_CASE("Constant Positive Value Function")
   CHECK(integral == Approx(15.37));
 }
 
+TEST_CASE("Genz2 Integral")
+{
+  double epsrel = 1.e-3;
+  double epsabs = 1.0e-12;
+  double true_value = 1.286889807581113e+13;
+  constexpr int ndim = 6;
+  F_2_6D integrand;
+  constexpr bool use_custom = false;
+
+  Workspace<double, ndim, use_custom> pagani;
+  quad::Volume<double, ndim> vol;
+  integration_result res = pagani.integrate(integrand, epsrel, epsabs, vol);
+  double integral = res.estimate;
+  CHECK(integral == Approx(true_value).epsilon(epsrel));
+}
+
+TEST_CASE("Genz3 Integral")
+{
+  double epsrel = 1.e-3;
+  double epsabs = 1.0e-12;
+  double true_value = 2.2751965817917756076e-10;
+  constexpr int ndim = 8;
+  F_3_8D integrand;
+  constexpr bool use_custom = false;
+  Workspace<double, ndim, use_custom> pagani;
+  quad::Volume<double, ndim> vol;
+  integration_result res = pagani.integrate(integrand, epsrel, epsabs, vol);
+  double integral = res.estimate;
+  CHECK(integral == Approx(true_value).epsilon(epsrel));
+}
+
+TEST_CASE("Genz4 Integral")
+{
+  double epsrel = 1.e-3;
+  double epsabs = 1.0e-12;
+  double true_value = 1.79132603674879e-06;
+  constexpr int ndim = 5;
+  F_4_5D integrand;
+  constexpr bool use_custom = false;
+
+  Workspace<double, ndim, use_custom> pagani;
+  quad::Volume<double, ndim> vol;
+  integration_result res = pagani.integrate(integrand, epsrel, epsabs, vol);
+  double integral = res.estimate;
+  CHECK(integral == Approx(true_value).epsilon(epsrel));
+}
+
+TEST_CASE("Genz5 Integral")
+{
+  double epsrel = 1.e-3;
+  double epsabs = 1.0e-12;
+  double true_value = 2.425217625641885e-06;
+  constexpr int ndim = 8;
+  F_5_8D integrand;
+  constexpr bool use_custom = false;
+  Workspace<double, ndim, use_custom> pagani;
+  quad::Volume<double, ndim> vol;
+  integration_result res = pagani.integrate(integrand, epsrel, epsabs, vol);
+  double integral = res.estimate;
+  CHECK(integral == Approx(true_value).epsilon(epsrel));
+}
+
+TEST_CASE("Genz6 Integral")
+{
+  double epsrel = 1.e-3;
+  double epsabs = 1.0e-12;
+  double true_value = 1.5477367885091207413e8;
+  constexpr int ndim = 6;
+  F_6_6D integrand;
+  constexpr bool use_custom = false;
+  Workspace<double, ndim, use_custom> pagani;
+  quad::Volume<double, ndim> vol;
+  integration_result res = pagani.integrate(integrand, epsrel, epsabs, vol);
+  double integral = res.estimate;
+  CHECK(integral == Approx(true_value).epsilon(epsrel));
+}
