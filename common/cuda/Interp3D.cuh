447ace376f4d64ce7d93b126a132dd390b858874
isaki001 <isaki001@odu.edu>
2023-02-20 18:28:20 -0500

    added Interp3D for cuda

---
+++ common/cuda/Interp3D.cuh
@@ -0,0 +1,290 @@
+#ifndef CUDA_INTERP_3D_CUH
+#define CUDA_INTERP_3D_CUH
+
+#include "cuda/pagani/quad/quad.h"
+#include "common/cuda/cudaArray.cuh"
+#include "common/cuda/cudaMemoryUtil.h"
+#include "common/cuda/cudaTimerUtil.h"
+#include "common/cuda/str_to_doubles.hh"
+#include <assert.h>
+#include <cstdlib>
+#include <iostream>
+#include <utility>
+
+namespace quad {
+  
+  class Interp3D {
+    // change names to xs, ys, zs to fit with y3_cluster_cpp::Interp3D
+    size_t size_x = 0;
+    size_t size_y = 0;
+	size_t size_z = 0;
+	
+    double* interpT = nullptr;
+	double* _zs = nullptr;
+    double* _ys = nullptr;  //y -> interpR
+    double* _xs = nullptr; //x -> interpC
+    
+    void
+    Alloc(size_t x, size_t y, size_t z)
+    {
+      if (x > 100000 || y > 100000 || z > 100000) {
+        std::cerr << "InterpD::Alloc called with x=" << x
+                  << " and y=" << y << " and z=" << z << '\n';
+        std::abort();
+      }
+      if (x * y * z > 1000000) {
+        std::cerr << "Interp3D::Alloc called with x=" << x
+                  << " and y=" << y << " and z=" << z << '\n';
+        std::abort();
+      }
+	  
+      size_x = x;
+      size_y = y;
+	    size_z = z;
+      
+      _xs = cuda_malloc<double>(x);
+      _ys = cuda_malloc<double>(y);
+      _zs = cuda_malloc<double>(z);
+      interpT = cuda_malloc<double>(x*y*z);
+      CudaCheckError();
+    }
+
+  public:
+    size_t
+    get_device_mem_footprint()
+    {
+      return 8 * (size_x * size_y * size_z + size_x + size_y + size_z);
+    }
+
+    size_t
+    get_device_mem_footprint() const
+    {
+      return 8 * (size_x * size_y * size_z + size_x + size_y + size_z);
+    }
+
+    void
+    swap(Interp3D& other)
+    {
+      std::swap(size_x, other.size_x);
+      std::swap(size_y, other.size_y);
+	    std::swap(size_z, other.size_z);
+
+      std::swap(interpT, other.interpT);
+      std::swap(_xs, other._xs);
+	    std::swap(_ys, other._ys);
+      std::swap(_zs, other._zs);
+    }
+
+    __host__ __device__
+    Interp3D()
+    {}
+
+    Interp3D(const Interp3D& source)
+    {
+      size_x = source.size_x;
+      size_y = source.size_y;
+	    size_z = source.size_z;
+      Alloc(size_x, size_y, size_z);
+      
+      cuda_memcpy_device_to_device<double>(interpT, source.interpT, size_x * size_y * size_z);
+      cuda_memcpy_device_to_device<double>(_xs, source._xs, size_x);
+      cuda_memcpy_device_to_device<double>(_ys, source._ys, size_y);
+	    cuda_memcpy_device_to_device<double>(_zs, source._zs, size_z);
+      CudaCheckError();
+    }
+
+    Interp3D&
+    operator=(Interp3D const& rhs)
+    {
+      Interp3D tmp(rhs);
+      CudaCheckError();
+      swap(tmp);
+      return *this;
+    }
+
+    
+    Interp3D(Interp3D&&) = delete;
+    Interp3D& operator=(Interp3D&&) = delete;
+
+    ~Interp3D()
+    {
+      cudaFree(_xs);
+      cudaFree(_ys);
+      cudaFree(_zs);
+	    cudaFree(interpT);
+    }
+
+    template <size_t M, size_t N, size_t S>
+    Interp3D(std::array<double, M> const& xs,
+             std::array<double, N> const& ys,
+			       std::array<double, S> const& zs,
+             std::array<double, N * M * S> const& vals)
+    {
+      CudaCheckError();
+      Alloc(M, N, S);
+	    cuda_memcpy_to_device<double>(_xs, xs.data(), M);
+      cuda_memcpy_to_device<double>(_ys, ys.data(), N);
+	    cuda_memcpy_to_device<double>(_zs, zs.data(), S);
+      cuda_memcpy_to_device<double>(interpT, vals.data(), N * M * S);
+      CudaCheckError();
+    }
+
+    Interp3D(double const* xs,
+             double const* ys,
+             double const* zs,
+             double const* vs,
+             size_t x_size,
+             size_t y_size,
+             size_t z_size)
+    {
+      CudaCheckError();
+      Alloc(x_size, y_size, z_size);
+      cuda_memcpy_to_device<double>(_xs, xs, x_size);
+      cuda_memcpy_to_device<double>(_ys, ys, y_size);
+      cuda_memcpy_to_device<double>(_zs, zs, z_size);
+      cuda_memcpy_to_device<double>(interpT, vs, x_size * y_size * z_size);
+      CudaCheckError();
+    }
+
+    Interp3D(std::vector<double> const& xs,
+             std::vector<double> const& ys,
+             std::vector<double> const& zs,
+             std::vector<double> const& vs)
+      : Interp3D(xs.data(), ys.data(), zs.data(), vs.data(), xs.size(), ys.size(), zs.size())
+    {
+      CudaCheckError();
+    }
+
+    __device__ bool
+    AreNeighbors(const double val,
+                 double* arr,
+                 const size_t leftIndex,
+                 const size_t RightIndex) const
+    {
+      return (arr[leftIndex] <= val && arr[RightIndex] >= val);
+    }
+	
+    __device__ void
+    FindNeighbourIndices(const double val,
+                         double* arr,
+                         const size_t size,
+                         size_t& leftI,
+                         size_t& rightI) const
+    {
+
+      size_t currentIndex = size / 2;
+      leftI = 0;
+      rightI = size - 1;
+      while (leftI <= rightI) {
+        currentIndex = (rightI + leftI) * 0.5;
+        if (AreNeighbors(val, arr, currentIndex, currentIndex + 1)) {
+          leftI = currentIndex;
+          rightI = currentIndex + 1;
+          return;
+        }
+
+        if (arr[currentIndex] > val) {
+          rightI = currentIndex;
+        } else {
+          leftI = currentIndex;
+        }
+      }
+    }
+
+    __device__ 
+    size_t 
+    index(size_t x, size_t y, size_t z)const{
+      return x + (size_x * y) + (size_x * size_y * z);
+    }
+
+    __device__ double 
+    operator()(double x, double y, double z) const
+    {
+      // y1, y2, x1, x2, are the indices of where to find the four neighbouring
+      // points in the z-table
+      size_t y0 = 0, y1 = 0;
+      size_t x0 = 0, x1 = 0;
+	    size_t z0 = 0, z1 = 0;
+
+	    //separately find two nearest values to each of x, y, z
+      FindNeighbourIndices(y, _ys, size_y, y0, y1);
+      FindNeighbourIndices(x, _xs, size_x, x0, x1);
+	    FindNeighbourIndices(z, _zs, size_z, z0, z1);
+		
+	    const double x_d = (x - _xs[x0]) / (_xs[x1] - _xs[x0]);
+	    const double y_d = (y - _ys[y0]) / (_ys[y1] - _ys[y0]);
+	    const double z_d = (z - _zs[z0]) / (_zs[z1] - _zs[z0]);
+	  
+	    //interpolate along x
+	    const double c00 = interpT[index(x0, y0, z0)] * (1 - x_d) + interpT[index(x1,y0,z0)] * x_d;
+	    const double c01 = interpT[index(x0, y0, z1)] * (1 - x_d) + interpT[index(x1, y0, z1)] * x_d;
+	    const double c10 = interpT[index(x0, y1, z0)] * (1 - x_d) + interpT[index(x1, y1, z0)] * x_d;
+	    const double c11 = interpT[index(x0, y1, z1)] * (1 - x_d) + interpT[index(x1, y1, z1)] * x_d;
+
+      //interpolate values along y
+      const double c0 = c00* (1 - y_d) + c10 * y_d;
+      const double c1 = c01 * (1 - y_d) + c11 * y_d;
+
+      //interpolate along z
+      const double f_xyz = c0 * (1 - z_d) + c1 * z_d;
+      return f_xyz;
+    }
+
+    __device__ double
+    min_x() const
+    {
+      return _xs[0];
+    }
+
+    __device__ double
+    max_x() const
+    {
+      return _xs[size_x - 1];
+    }
+
+    __device__ double
+    min_y() const
+    {
+      return _ys[0];
+    }
+
+    __device__ double
+    max_y() const
+    {
+      return _ys[size_y - 1];
+    }
+	
+	__device__ double
+    min_z() const
+    {
+      return _zs[0];
+    }
+	
+    __device__ double
+    max_z() const
+    {
+      return _zs[size_z - 1];
+    }
+	
+    __device__ double
+    do_clamp(double v, double lo, double hi) const
+    {
+      assert(!(hi < lo));
+      return (v < lo) ? lo : (hi < v) ? hi : v;
+    }
+	
+    __device__ double
+    eval(double x, double y, double z) const
+    {
+      return this->operator()(x, y, z);
+    };
+
+    __device__ double
+    clamp(double x, double y, double z) const
+    {
+      return eval(do_clamp(x, min_x(), max_x()), do_clamp(y, min_y(), max_y()), do_clamp(z, min_z(), max_z()));
+    }
+  };
+}
+
+#endif
---
+++ test/cuda/common/Interpolation3D.cu
@@ -0,0 +1,181 @@
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+#include "common/cuda/Interp3D.cuh"
+#include "common/cuda/cudaMemoryUtil.h"
+
+#include <array>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+__global__ void
+gEvaluate(quad::Interp3D f, double x, double y, double z, double* result)
+{
+  *result = f(x, y, z);
+}
+
+double
+Evaluate(quad::Interp3D f, double x, double y, double z)
+{
+  double* result = quad::cuda_malloc_managed<double>(1);
+  gEvaluate<<<1, 1>>>(f, x, y, z, result);
+  cudaDeviceSynchronize();
+  double hResult = *result;
+  cudaFree(result);
+  return hResult;
+}
+
+__global__ void
+gClamp(quad::Interp3D f, double x, double y, double z, double* result)
+{
+  *result = f.clamp(x, y, z);
+}
+
+double
+clamp(quad::Interp3D f, double x, double y, double z)
+{
+  double* result = quad::cuda_malloc_managed<double>(1);
+  gClamp<<<1, 1>>>(f, x, y, z, result);
+  cudaDeviceSynchronize();
+  double hResult = *result;
+  cudaFree(result);
+  return hResult;
+}
+
+struct Indexer{
+  Indexer(size_t xs, size_t ys, size_t zs):_xs(xs), _ys (ys), _zs(zs){}
+  
+  size_t operator()(double x, double y, double z){
+    return x + (_xs * y) + (_xs * _ys * z);
+  }
+
+private:
+
+  size_t _xs = 0;
+  size_t _ys = 0;
+  size_t _zs = 0;
+};
+
+void
+test_clamp_interface()
+{
+  constexpr std::size_t nx = 3;
+  constexpr std::size_t ny = 2;
+  constexpr std::size_t nz = 4;
+
+  std::array<double, nx> xs = {1., 2., 3.};
+  std::array<double, ny> ys = {4., 5.};
+  std::array<double, nz> zs = {4., 5., 6., 7.};
+
+  std::array<double, ny * nx * nz> vs;
+  Indexer index(nx, ny, nz);
+  auto fxyz = [](double x, double y, double z) { return 2*x + 3*y + 4*z;};
+
+  for(std::size_t k = 0; k != nz; ++k){
+    double z = zs[k];
+    for (std::size_t i = 0; i != nx; ++i) {
+      double x = xs[i];
+      for (std::size_t j = 0; j != ny; ++j) {
+        double y = ys[j];
+        vs[index(i, j, k)] = fxyz(x, y, z);
+      } 
+   }
+  }
+
+  quad::Interp3D f(xs, ys, zs, vs);
+
+  SECTION("interpolation works")
+  {
+    double x = 2.5;
+    double y = 4.5;
+    double z = 6.3;
+    double true_result = fxyz(x, y, z);
+    double interpResult = Evaluate(f, x, y, z);
+    CHECK(interpResult == true_result);
+  }
+
+  SECTION("extrapolation gets clamped")
+  {
+    double clampRes = clamp(f, 0., 4.5, 6.3);
+    double interpResult = clamp(f, 1., 4.5, 6.3);
+    CHECK(clampRes == interpResult); // to the left x
+
+    clampRes =     clamp(f, 4., 4.5, 6.3); // to the right x
+    interpResult = clamp(f, 3., 4.5, 6.3);
+    CHECK(clampRes == interpResult);
+
+    clampRes =     clamp(f, 2., 3., 6.3); //to the left y 
+    interpResult = clamp(f, 2., 4., 6.3);
+    CHECK(clampRes == interpResult);
+
+    clampRes = clamp(f, 2., 5.5, 6.3);  //to the right y
+    interpResult = clamp(f, 2., 5., 6.3);
+    CHECK(clampRes == interpResult);
+
+    clampRes = clamp(f, 1.1, 4.2, 3.1); // to the left z
+    interpResult = clamp(f, 1.1, 4.2, 4.); 
+    CHECK(clampRes == interpResult);
+
+    clampRes = clamp(f, 1.1, 4.2, 7.4); //to the right z
+    interpResult = clamp(f, 1.1, 4.2, 7.);
+    CHECK(clampRes == interpResult);
+
+    clampRes = clamp(f, 0., 3.1, 3.1);
+    interpResult = clamp(f, 1., 4., 4.);
+    CHECK(clampRes == interpResult);
+
+    clampRes = clamp(f, 3.5, 5.6, 7.3);
+    interpResult = clamp(f, 3., 5., 7.);
+    CHECK(clampRes == interpResult);
+  }
+}
+
+void
+test_interpolation_at_knots()
+{
+  constexpr std::size_t nx = 3;
+  constexpr std::size_t ny = 2;
+  constexpr std::size_t nz = 4;
+
+  std::array<double, nx> xs = {1., 2., 3.};
+  std::array<double, ny> ys = {4., 5.};
+  std::array<double, nz> zs = {4., 5., 6., 7.};
+
+  std::array<double, ny * nx * nz> vs;
+  Indexer index(nx, ny, nz);
+  auto fxyz = [](double x, double y, double z) { return 2*x + 3*y + 4*z;};
+
+  for(std::size_t k = 0; k != nz; ++k){
+    double z = zs[k];
+    for (std::size_t i = 0; i != nx; ++i) {
+      double x = xs[i];
+      for (std::size_t j = 0; j != ny; ++j) {
+        double y = ys[j];
+        vs[index(i, j, k)] = fxyz(x, y, z);
+      } 
+   }
+  }
+
+  quad::Interp3D f(xs, ys, zs, vs);
+  for(std::size_t k = 0; k != nz; ++k){
+    double z = zs[k];
+    for (std::size_t i = 0; i != nx; ++i) {
+      double x = xs[i];
+      for (std::size_t j = 0; j != ny; ++j) {
+        double y = ys[j];
+        CHECK(Evaluate(f, x, y, z) == fxyz(x, y, z));
+      } 
+   }
+  }
+}
+
+TEST_CASE("clamp interface works")
+{
+  test_clamp_interface();
+}
+
+TEST_CASE("Interp2D exact at knots")
+{
+  test_interpolation_at_knots();
+}
